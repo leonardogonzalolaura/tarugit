0ce45cbcde0d7fabe01df612df3ea1f14e81af3b
leonardogonzalolaura <glleonardodaniel@gmail.com>
2026-06-19 22:17:37 -0500

    feat: add ico branch

--- src/components/BranchSelector.tsx
+++ src/components/BranchSelector.tsx
@@ -427,7 +427,9 @@ export function BranchSelector({
         >
           <div className="branch-selector-item-info">
             <span className="branch-selector-item-icon">
-              {isCurrent ? '●' : branch.is_remote ? '☁️' : '○'}
+              <svg viewBox="0 0 16 16" width="12" height="12" fill="currentColor">
+                <path d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.25 2.25 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.5 2.5 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z"/>
+              </svg>
             </span>
             <span className="branch-selector-item-name" style={{ wordBreak: 'break-all' }}>
               {branch.name}
@@ -508,7 +510,9 @@ export function BranchSelector({
           title="Cambiar de rama"
           disabled={isOperating}
         >
-          <span className="branch-dot" />
+          <svg className="branch-icon" viewBox="0 0 16 16" width="14" height="14" fill="currentColor">
+            <path d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.25 2.25 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.5 2.5 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z"/>
+          </svg>
           <span className="branch-selector-current">
             {displayBranch}
           </span>
